d1886906e2ca21e28a5a718c6f49d2e202f3eea6
johnmmcparland <johnmmcparland@gmail.com>
2014-06-09 21:08:41 +0100

    Deleting widgetsets.
    Updating pom

--- ch02_customlayout/src/main/webapp/VAADIN/widgetsets/com.mcparland.john.AppWidgetSet/deferredjs/BEEC4D7F8F76A4BA5DB009A0DB537FAB/1.cache.js
+++
@@ -1,1 +0,0 @@
-$wnd.com_mcparland_john_AppWidgetSet.runAsyncCallback1('i8(1338,1,jnd);_.nc=function g$b(){wPb((!rPb&&(rPb=new xPb),rPb),this.b.e)};ubd(Vh)(1);\n//# sourceURL=com.mcparland.john.AppWidgetSet-1.js\n')
