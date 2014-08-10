b793c97765b1b1c979622eced371386b5603cd9f
johnmmcparland <johnmmcparland@gmail.com>
2014-08-10 21:58:43 +0100

    URL view params
    Updated the example slightly to use URL parameters.

--- ch02_navigator/src/main/java/com/mcparland/john/OrdersView.java
+++ ch02_navigator/src/main/java/com/mcparland/john/OrdersView.java
@@ -18,6 +18,26 @@
  */
 package com.mcparland.john;
 
+/*
+ * #%L
+ * Chapter 2: Navigator
+ * %%
+ * Copyright (C) 2014 John McParland
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.ui.Label;
@@ -35,7 +55,7 @@ public class OrdersView extends VerticalLayout implements View {
     /**
      * Name of this view.
      */
-    public static final String VIEW_NAME = "Orders";
+    public static final String VIEW_NAME = "orders";
 
     /**
      * Create a OrdersView.
@@ -56,6 +76,10 @@ public class OrdersView extends VerticalLayout implements View {
     public void enter(ViewChangeEvent event) {
         // If you need to connect to db or otherwise customise upon navigation,
         // do it here rather than in the constructor.
+
+        // Do fun stuff with parameters
+        String params = event.getParameters();
+        addComponent(new Label("Params: " + params));
     }
 
 }
