1a07b7220a3e80ada1a911755e1fe7bcbda96d73
John McParland <johnmmcparland@gmail.com>
2015-01-03 21:37:19 +0000

    Chapter 3: Flot Chart Example complete
    Done.

---
+++ Chapter03/flotChart/src/main/java/com/mcparland/john/vaadin_cookbook/FlotChart.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.mcparland.john.vaadin_cookbook;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import com.vaadin.annotations.JavaScript;
+import com.vaadin.ui.AbstractJavaScriptComponent;
+
+/**
+ * A FlotChart.
+ * 
+ * @author John McParland.
+ *
+ */
+@JavaScript({ "jquery.min-1.11.2.js", "jquery.flot-0.8.3.js", "flot_connector.js" })
+public class FlotChart extends AbstractJavaScriptComponent {
+
+    /**
+     * The serialVersionUID.
+     */
+    private static final long serialVersionUID = -4887074161683017185L;
+
+    /**
+     * Create a FlotChart.
+     *
+     */
+    public FlotChart() {
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.vaadin.ui.AbstractJavaScriptComponent#getState()
+     */
+    public FlotChartState getState() {
+        return (FlotChartState) super.getState();
+    }
+
+    /**
+     * Sets the data. Please use in preference to
+     * {@link AbstractJavaScriptComponent#setData(Object)}.
+     * 
+     * @param source
+     *            the JSON array which is the source of the data.
+     */
+    public void setData(String source) {
+        JSONArray data = null;
+        try {
+            data = new JSONArray(source);
+            getState().setData(data);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ Chapter03/flotChart/src/main/java/com/mcparland/john/vaadin_cookbook/FlotChartState.java
@@ -0,0 +1,70 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.mcparland.john.vaadin_cookbook;
+
+import org.json.JSONArray;
+
+import com.vaadin.shared.ui.JavaScriptComponentState;
+
+/**
+ * A FlotChartState.
+ * 
+ * @author John McParland.
+ *
+ */
+public class FlotChartState extends JavaScriptComponentState {
+
+    /**
+     * The serialVersionUID.
+     */
+    private static final long serialVersionUID = 1345528294577585084L;
+
+    /**
+     * The data.
+     */
+    private JSONArray data = null;
+
+    /**
+     * Create a FlotChartState.
+     *
+     */
+    public FlotChartState() {
+
+    }
+
+    /**
+     * Get the data.
+     * 
+     * @return the data.
+     */
+    public JSONArray getData() {
+        return data;
+    }
+
+    /**
+     * Set the data.
+     * 
+     * @param data
+     *            the data to set.
+     */
+    public void setData(JSONArray data) {
+        this.data = data;
+    }
+
+}
